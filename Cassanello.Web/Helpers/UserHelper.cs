1d9f64aaa5da7e600200cea3f1166d9077b03a20
Mario Riveros <marioriveros@gmail.com>
2020-02-17 08:20:39 -0300

    Agregue clase UserHelpers

---
+++ Cassanello.Web/Helpers/UserHelper.cs
@@ -0,0 +1,51 @@
+﻿using Cassanello.Web.Datos.Entidades;
+using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Cassanello.Web.Helpers
+{
+    public class UserHelper : IUserHelper
+    {
+        private readonly UserManager<Usuario> _userManager;
+        private readonly RoleManager<IdentityRole> _roleManager;
+
+        public UserHelper(UserManager<Usuario> userManager,
+            RoleManager<IdentityRole> roleManager)
+        {
+            _userManager = userManager;
+            _roleManager = roleManager;
+        }
+
+
+
+
+        public Task<IdentityResult> AddUserAsync(Usuario user, string password)
+        {
+            return await _userManager.CreateAsync(user, password);
+
+        }
+
+        public Task AddUserToRoleAsync(Usuario user, string roleName)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task CheckRoleAsync(string roleName)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Usuario> GetUserByEmailAsync(string email)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> IsUserInRoleAsync(Usuario user, string roleName)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
